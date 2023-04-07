2e05e57a1063ffaee257cf9e09afb38dccb3cd1d
Vikram Kumar Saw <vicky28102000@gmail.com>
2023-04-07 23:20:37 +0530

    chaining models

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const generateToken = require("../config/generateToken");
-const User = require("../Models/userModel");
+const User = require("../Models/usermodel");
 
 const allUsers = asyncHandler(async (req, res) => {
   const keyword = req.query.search
